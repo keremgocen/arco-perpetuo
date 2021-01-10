2986bcf8ed46177717e288e302f4072bf5f7f1c4
keremgocen <keremgocen@gmail.com>
2021-01-10 19:31:34 +0300

    add disabled styles for input

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,43 +1,72 @@
-import React from 'react';
+import React from "react";
 import {
-    createStyles,
-    fade,
-    Theme,
-    makeStyles,
-} from '@material-ui/core/styles';
-import TextField, { TextFieldProps } from '@material-ui/core/TextField';
-import { OutlinedInputProps } from '@material-ui/core/OutlinedInput'
+  createStyles,
+  fade,
+  Theme,
+  makeStyles,
+  withStyles,
+} from "@material-ui/core/styles";
+import TextField, { TextFieldProps } from "@material-ui/core/TextField";
+import { OutlinedInputProps } from "@material-ui/core/OutlinedInput";
 
-const useStylesReddit = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            border: '1px solid #e2e2e1',
-            overflow: 'hidden',
-            borderRadius: 4,
-            backgroundColor: '#fcfcfb',
-            transition: theme.transitions.create(['border-color', 'box-shadow']),
-            '&:hover': {
-                backgroundColor: '#fff',
-            },
-            '&$focused': {
-                backgroundColor: '#fff',
-                boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
-                borderColor: theme.palette.primary.main,
-            },
-        },
-        focused: {},
-    }),
+const useStylesCustom = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      //   border: "1px solid #e2e2e1",
+      overflow: "hidden",
+      borderRadius: 4,
+      //   backgroundColor: theme.palette.primary.main,
+      transition: theme.transitions.create(["border-color", "box-shadow"]),
+      "&:hover": {
+        backgroundColor: theme.palette.primary.main,
+      },
+      "&$focused": {
+        backgroundColor: "#fff",
+        boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+    focused: {},
+  })
 );
 
-const CustomInput = (props: TextFieldProps) => {
-    const classes = useStylesReddit();
+const CustomTextField = (props: TextFieldProps) => {
+  const classes = useStylesCustom();
 
-    return (
-        <TextField
-            InputProps={{ classes, disableUnderline: true } as Partial<OutlinedInputProps>}
-            {...props}
-        />
-    );
-}
+  return (
+    <TextField
+      InputProps={{ classes } as Partial<OutlinedInputProps>}
+      {...props}
+    />
+  );
+};
+
+const CustomInput = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      //   backgroundColor: theme.palette.primary.main,
+      //   "& label.Mui-focused": {
+      //     color: "green",
+      //   },
+      //   "& .MuiInput-underline:after": {
+      //     borderBottomColor: "green",
+      //   },
+      "& .MuiOutlinedInput-root": {
+        //     "& fieldset": {
+        //       borderColor: "red",
+        //     },
+        //     "&:hover fieldset": {
+        //       borderColor: "yellow",
+        //     },
+        //     "&.Mui-focused fieldset": {
+        //       borderColor: "green",
+        //     },
+        "&.Mui-disabled": {
+          backgroundColor: theme.palette.primary.main,
+        },
+      },
+    },
+  })
+)(TextField);
 
 export default CustomInput;
--- components/Pips.tsx
+++ components/Pips.tsx
@@ -2,70 +2,53 @@ import React from "react";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
+import CustomInput from "./CustomInput";
+import { grey } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      display: 'flex',
-      flexFlow: 'row wrap',
-      justifyContent: 'space-around',
-      padding: 50,
+      display: "flex",
+      flexFlow: "row wrap",
+      justifyContent: "space-around",
+      padding: 30,
     },
     item: {
       margin: theme.spacing(0),
+      width: 30,
+      height: 30,
     },
     box: {
+      backgroundColor: "lightgrey",
       borderRadius: 4,
-      minWidth: 30,
-      minHeight: 30,
+      minWidth: 10,
+      minHeight: 10,
     },
   })
 );
 
 const InputGrid = () => {
+  const text = "12";
   const classes = useStyles();
   return (
     <Grid container spacing={1} className={classes.root}>
-      <Grid item xs={2} sm={2} className={classes.item}>
-        <Box
-          bgcolor="primary.main"
-          color="primary.contrastText"
-          p={2}
-          className={classes.box}
-        />
-      </Grid>
-      <Grid item xs={2} sm={2} className={classes.item}>
-        <Box
-          bgcolor="primary.main"
-          color="secondary.contrastText"
-          p={2}
-          className={classes.box}
-        />
-      </Grid>
-      <Grid item xs={2} sm={2} className={classes.item}>
-        <Box
-          bgcolor="primary.main"
-          color="error.contrastText"
-          p={2}
-          className={classes.box}
-        />
-      </Grid>
-      <Grid item xs={2} sm={2} className={classes.item}>
-        <Box
-          bgcolor="primary.main"
-          color="warning.contrastText"
-          p={2}
-          className={classes.box}
-        />
-      </Grid>
-      <Grid item xs={2} sm={2} className={classes.item}>
-        <Box
-          bgcolor="primary.main"
-          color="warning.contrastText"
-          p={2}
-          className={classes.box}
-        />
-      </Grid>
+      {[0, 1, 2, 3, 4].map((value) => (
+        <Grid item xs={2} sm={2} className={classes.item}>
+          {/* <Box
+            bgcolor={grey}
+            // color="primary.contrastText"
+            className={classes.box}
+          > */}
+          <CustomInput
+            variant="outlined"
+            id="custom-css-outlined-input"
+            disabled={text.length > value}
+            className={classes.box}
+            autoFocus={false}
+          />
+          {/* </Box> */}
+        </Grid>
+      ))}
     </Grid>
   );
 };
