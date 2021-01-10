0f6dbf9f426d14e1e45fe7a8784879f5bcf58570
keremgocen <keremgocen@gmail.com>
2021-01-10 19:49:22 +0300

    fix build error

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,66 +1,10 @@
-import React from "react";
-import {
-  createStyles,
-  fade,
-  Theme,
-  makeStyles,
-  withStyles,
-} from "@material-ui/core/styles";
-import TextField, { TextFieldProps } from "@material-ui/core/TextField";
-import { OutlinedInputProps } from "@material-ui/core/OutlinedInput";
-
-const useStylesCustom = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      //   border: "1px solid #e2e2e1",
-      overflow: "hidden",
-      borderRadius: 4,
-      //   backgroundColor: theme.palette.primary.main,
-      transition: theme.transitions.create(["border-color", "box-shadow"]),
-      "&:hover": {
-        backgroundColor: theme.palette.primary.main,
-      },
-      "&$focused": {
-        backgroundColor: "#fff",
-        boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
-        borderColor: theme.palette.primary.main,
-      },
-    },
-    focused: {},
-  })
-);
-
-const CustomTextField = (props: TextFieldProps) => {
-  const classes = useStylesCustom();
-
-  return (
-    <TextField
-      InputProps={{ classes } as Partial<OutlinedInputProps>}
-      {...props}
-    />
-  );
-};
+import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
 const CustomInput = withStyles((theme: Theme) =>
   createStyles({
     root: {
-      //   backgroundColor: theme.palette.primary.main,
-      //   "& label.Mui-focused": {
-      //     color: "green",
-      //   },
-      //   "& .MuiInput-underline:after": {
-      //     borderBottomColor: "green",
-      //   },
       "& .MuiOutlinedInput-root": {
-        //     "& fieldset": {
-        //       borderColor: "red",
-        //     },
-        //     "&:hover fieldset": {
-        //       borderColor: "yellow",
-        //     },
-        //     "&.Mui-focused fieldset": {
-        //       borderColor: "green",
-        //     },
         "&.Mui-disabled": {
           backgroundColor: theme.palette.primary.main,
         },
--- components/Pips.tsx
+++ components/Pips.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import CustomInput from "./CustomInput";
-import { grey } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -34,11 +32,6 @@ const InputGrid = () => {
     <Grid container spacing={1} className={classes.root}>
       {[0, 1, 2, 3, 4].map((value) => (
         <Grid item xs={2} sm={2} className={classes.item}>
-          {/* <Box
-            bgcolor={grey}
-            // color="primary.contrastText"
-            className={classes.box}
-          > */}
           <CustomInput
             variant="outlined"
             id="custom-css-outlined-input"
@@ -46,7 +39,6 @@ const InputGrid = () => {
             className={classes.box}
             autoFocus={false}
           />
-          {/* </Box> */}
         </Grid>
       ))}
     </Grid>
