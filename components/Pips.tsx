fd4ceb7289b560477e875e18486b4aa7d753758a
keremgocen <keremgocen@gmail.com>
2021-01-14 01:17:33 +0300

    backend connection, env vars, popper

--- components/Pips.tsx
+++ components/Pips.tsx
@@ -1,8 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useRef } from "react";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import { PinCodeContext } from '../hooks/PinCodeProvider'
+import axios from 'axios';
+import Popper from '@material-ui/core/Popper';
+import Typography from '@material-ui/core/Typography';
+import Fade from '@material-ui/core/Fade';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -10,46 +15,74 @@ const useStyles = makeStyles((theme: Theme) =>
       display: "flex",
       flexFlow: "row wrap",
       justifyContent: "space-around",
-      padding: 30,
+      padding: 40,
     },
     item: {
       margin: theme.spacing(0),
-      width: 30,
-      height: 30,
     },
     active: {
       backgroundColor: theme.palette.primary.main,
       borderRadius: 4,
-      minWidth: 10,
-      minHeight: 10,
     },
     dormant: {
       backgroundColor: 'lightgrey',
       borderRadius: 4,
-      minWidth: 10,
-      minHeight: 10,
+    },
+    popper: {
+      border: '1px solid',
+      padding: theme.spacing(1),
     },
   })
 );
 
 const InputGrid = () => {
-  const { pinCode } = useContext(PinCodeContext);
   const classes = useStyles();
+  const { pinCode } = useContext(PinCodeContext);
+  const [popperText, setPopperText] = useState<string>('');
+  const [popperColor, setPopperColor] = useState<string>('');
+  const PINCODE_URL = process.env.API_PINCODE_URL;
+  const localEl = useRef(null);
+
+  if (pinCode && pinCode.length === 5 && PINCODE_URL) {
+    const formData = new FormData();
+    formData.append('pin_code', pinCode);
+    axios.post(PINCODE_URL, formData)
+      .then(response => {
+        setPopperText(`${response.status} ${response.statusText}`);
+        setPopperColor('lightgreen');
+      }).catch(error => {
+        setPopperText(`Error ${error.response?.status}`);
+        setPopperColor(error.response ? 'yellow' : 'red');
+      });
+  }
+
+  const open = Boolean(popperText) && !!pinCode && pinCode.length === 5;
+  const id = open ? 'simple-popper' : undefined;
 
   return (
     <Grid container spacing={1} className={classes.root}>
       {[0, 1, 2, 3, 4].map((value) => (
-        <Grid key={value} item xs={2} sm={2} className={classes.item}>
+        <Grid key={value} sm={2} xs={2} item className={classes.item} ref={localEl}>
           <TextField
             variant="outlined"
-            id="custom-css-outlined-input"
+            id={"custom-css-outlined-input" + value}
             disabled={true}
             className={pinCode && pinCode.length > value ? classes.active : classes.dormant}
             autoFocus={false}
           />
         </Grid>
-      ))}
-    </Grid>
+      ))
+      }
+      <Popper id={id} open={open} anchorEl={localEl.current} transition placement="top-end">
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Paper>
+              <Typography className={classes.popper} style={{ backgroundColor: popperColor }}>{popperText}</Typography>
+            </Paper>
+          </Fade>
+        )}
+      </Popper>
+    </Grid >
   );
 };
 
